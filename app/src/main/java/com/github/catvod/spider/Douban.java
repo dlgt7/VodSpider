a0e92985e20c1e0920d0b7d12cc770c6200a6418
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-07-03 14:18:45 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Douban.java
+++ app/src/main/java/com/github/catvod/spider/Douban.java
@@ -9,6 +9,7 @@ import com.github.catvod.bean.Vod;
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.net.OkHttp;
 import com.github.catvod.utils.Json;
+import com.google.gson.JsonElement;
 
 import org.json.JSONArray;
 import org.json.JSONException;
