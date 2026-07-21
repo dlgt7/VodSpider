8d8802308be75079eb37e5a166738f2f7fc42951
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-07-21 10:03:22 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/merge/z/v3.java
+++ app/src/main/java/com/github/catvod/spider/merge/z/v3.java
@@ -1,1 +1,8 @@
-q
+package com.github.catvod.spider.merge.z;
+
+/** Configuration update sink invoked from {@link com.github.catvod.spider.FishConfig#d(String, String, String)}. */
+public class v3 {
+
+    public static void V0(String[] a0, String[] a1, String[] a2, String p1, String p2) {
+    }
+}
