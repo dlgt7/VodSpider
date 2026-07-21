4d7e8ed0edd570a1ed9c72b108281b1a3269d259
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-07-21 10:01:20 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/merge/f/c.java
+++ app/src/main/java/com/github/catvod/spider/merge/f/c.java
@@ -1,1 +1,16 @@
-1
+package com.github.catvod.spider.merge.f;
+
+import com.google.gson.JsonObject;
+
+/** Account configuration handler used by {@link com.github.catvod.spider.FishConfig#b()}. */
+public class c {
+
+    public c() {
+    }
+
+    public void b(JsonObject json) {
+    }
+
+    public void c(JsonObject json) {
+    }
+}
