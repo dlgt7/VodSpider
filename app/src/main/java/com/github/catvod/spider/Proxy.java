04fb1264b4b327eba77e18dfa74b291317e8e61b
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-07-25 19:02:09 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -68,13 +68,6 @@ public class Proxy {
                     return video(params);
                 case "audio":
                     return audio(params);
-                case "yueyue":
-                    try {
-                        return YueYue.ProxyHandler.c(params);
-                    } catch (Throwable t) {
-                        String msg = "悦悦代理" + (t.getMessage() != null ? t.getMessage() : t.getClass().getSimpleName());
-                        return error(500, msg);
-                    }
                 default:
                     return null;
             }
@@ -209,7 +202,7 @@ public class Proxy {
         }
     }
 
-    static Object[] error(int code, String message) {
+    private static Object[] error(int code, String message) {
         return new Object[]{code, "text/plain; charset=utf-8", new ByteArrayInputStream(message.getBytes(StandardCharsets.UTF_8))};
     }
 
