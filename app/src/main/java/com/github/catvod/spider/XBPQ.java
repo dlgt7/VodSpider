4517cc491014316e9d547a8b547a0d186112d264
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-08-23 14:20:05 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/XBPQ.java
+++ app/src/main/java/com/github/catvod/spider/XBPQ.java
@@ -5579,6 +5579,7 @@ public class XBPQ extends Spider {
 
         // 入队，串行等待
         SingletonWebView singleton = SingletonWebView.getInstance();
+        singleton.init(context, this); // P5: 注入 owner，供内部调用实例方法
         Req req = new Req(url, ua, headers, extraWait, useReadyPoll, readySelector, readyTimeout);
         singleton.enqueue(req);
 
