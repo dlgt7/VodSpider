fa8920d27244d365325bd005ddb832a6f3300fb1
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-08-25 06:50:13 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/xbpq/utils/HtmlNodeHelper.java
+++ app/src/main/java/com/github/catvod/spider/xbpq/utils/HtmlNodeHelper.java
@@ -1,1 +1,25 @@
-1
+package com.github.catvod.spider.xbpq.utils;
+
+/**
+ * HTML文本清理工具类
+ */
+public class HtmlNodeHelper {
+
+    /**
+     * 移除HTML标签并清理空白字符
+     */
+    public static String cleanText(String html) {
+        if (html == null) return "";
+        return html.replace("\r\n", "")
+                .replace("\n", "")
+                .replaceAll("<[^>]+?>", "")
+                .replaceAll("\\s+", " ")
+                .replace("&nbsp;", "")
+                .replace("&emsp;", "")
+                .replace("&amp;", "&")
+                .replace("&lt;", "<")
+                .replace("&gt;", ">")
+                .replace("&quot;", "\"")
+                .trim();
+    }
+}
